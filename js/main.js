59b1333a30190f7e0980a27da8014a8bdee2a981
yaoquan <E0318675@u.nus.edu>
2020-12-17 02:50:04 +0800

    adding responsive layouts, wip

--- js/main.js
+++ js/main.js
@@ -1,71 +1,83 @@
-// variables
-var loader = document.querySelector('.loader');
-var header = document.querySelector('.header');
-var nve150 = -150
+// // variables
+// var loader = document.querySelector('.loader');
+// var header = document.querySelector('.header');
 
-var teaMojs = new mojs.Html({
-    el: ".tea",
-    y: { 0: -100 },
-    duration: 1800
-})
+// const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+// const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 
-var textMojs = new mojs.Html({
-    el: ".loading-text",
-    x: { 0: -100 },
-    duration: 1800
-})
+// var teaMojs = new mojs.Html({
+//     el: ".tea",
+//     y: { 0: -100 },
+//     duration: 1800
+// })
 
-var headerStartMojs = new mojs.Html({
-    el: ".header",
-    y: { 100: 0 },
-    duration: 500
-})
+// var textMojs = new mojs.Html({
+//     el: ".loading-text",
+//     x: { 0: -100 },
+//     duration: 1800
+// })
 
-var navMojs = new mojs.Html({
-    el: ".nav",
-    x: { 0: 150 },
-    duration: 500
-})
+// var headerStartMojs = new mojs.Html({
+//     el: ".header",
+//     y: { 100: 0 },
+//     duration: 500
+// })
 
-function init() {
-    setTimeout(() => {
-        teaMojs.play()
-        textMojs.play()
-        fade(loader)
-    }, 1800);
-    setTimeout(() => {
-        headerStartMojs.play()
-        navMojs.play()
-        unfade(header)
-    }, 3300);
-}
+// var navMojs = new mojs.Html({
+//     el: ".nav",
+//     x: { 0: 150 },
+//     duration: 500
+// })
 
-//functions
-function fade(element) {
-    var op = 1;  // initial opacity
-    var timer = setInterval(function () {
-        if (op <= 0.1) {
-            clearInterval(timer);
-            element.style.display = 'none';
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.1;
-    }, 50);
-}
+// var navMobileMojs = new mojs.Html({
+//     el: ".nav",
+//     y: { 0: -150 },
+//     duration: 500
+// })
 
-function unfade(element) {
-    var op = 0.1;  // initial opacity
-    element.style.display = 'block';
-    var timer = setInterval(function () {
-        if (op >= 1) {
-            clearInterval(timer);
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.1;
-    }, 10);
-}
+// function init() {
+//     setTimeout(() => {
+//         teaMojs.play()
+//         textMojs.play()
+//         fade(loader)
+//     }, 1800);
+//     setTimeout(() => {
+//         headerStartMojs.play()
+//         if (vw > 950) {
+//             navMojs.play()
+//         } else {
+//             navMobileMojs.play()
+//         }
+//         unfade(header)
+//     }, 3300);
+// }
 
-init();
+// //functions
+// function fade(element) {
+//     var op = 1;  // initial opacity
+//     var timer = setInterval(function () {
+//         if (op <= 0.1) {
+//             clearInterval(timer);
+//             element.style.display = 'none';
+//         }
+//         element.style.opacity = op;
+//         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+//         op -= op * 0.1;
+//     }, 50);
+// }
+
+// function unfade(element) {
+//     var op = 0.1;  // initial opacity
+//     element.style.display = 'block';
+//     var timer = setInterval(function () {
+//         if (op >= 1) {
+//             clearInterval(timer);
+//         }
+//         element.style.opacity = op;
+//         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+//         op += op * 0.1;
+//     }, 10);
+// }
+
+// init();
 
