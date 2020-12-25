4f6322b0bb09f0f3e6a680a38f907c4250ae6107
yaoquan <E0318675@u.nus.edu>
2020-12-26 01:01:31 +0800

    moved all transitions to js. working on idea for home page (idea from tom riddle notebook)

--- js/main.js
+++ js/main.js
@@ -1,53 +1,94 @@
-const tl = gsap.timeline({ defaults: { ease: "power2.out" } })
+// global variables
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+const home = document.querySelector('#first')
+const showcase = document.querySelector('#second')
+const skills = document.querySelector('#third')
+const about = document.querySelector('#fourth')
 
-// loading page
-tl.to(".loader", { y: -150, opacity: 0, duration: 1, delay: 1 })
-tl.to(".page#p0", { opacity: 0, duration: 0.1 })
-tl.to(".loader", { height: 0, zIndex: -100, duration: 0.1, delay: 0.2 })
-tl.to(".page#p0", { height: 0, zIndex: -100, duration: 0.1, delay: 0.2 })
+// initial animation start
 
-// initial-header
-tl.from("#initial-header", { y: "100px", opacity: 0 })
-tl.to("#initial-header", { y: "0", opacity: 1, duration: 2 })
+// variables
+const initial_tl = gsap.timeline({ defaults: { ease: "power2.out" } })
 
+function home_func() {
+    var typed = new Typed('#initial-text', {
+        strings: [
+            'I\'m Yao Quan',
+            "Student/Wannabe Web Developer",
+            "Feel free to type here and talk to me!"
+        ],
+        startDelay: 4500,
+        typeSpeed: 30,
+        backDelay: 1500,
+        backSpeed: 10,
+        loop: false,
+        showCursor: false
+    })
+}
 
+function loading_animation() {
+    // loading page
+    initial_tl.to(".loader", { y: -150, opacity: 0, duration: 1, delay: 1 })
+    initial_tl.to(".page#p0", { opacity: 0, duration: 0.1 })
+    initial_tl.to(".loader", { height: 0, zIndex: -100, duration: 0.1, delay: 0.2 })
+    initial_tl.to(".page#p0", { height: 0, zIndex: -100, duration: 0.1, delay: 0.2 })
 
-// .page#p0
-// animation-name: fadeOut, disappear;
-// animation-duration: 1s, 0.1s;
-// animation-delay: 2s, 3.1s;
-// animation-timing-function: ease-in, linear;
-// animation-fill-mode: forwards, forwards;
+    // initial-header
+    initial_tl.from("#initial-header", { y: "100px", opacity: 0 }, "initial-header")
+    initial_tl.to("#initial-header", { y: "0px", opacity: 1, duration: 2 })
 
-// .loader
+    // nav-bar
+    if (vw > 950) {
+        console.log(vw)
+        initial_tl.from(".nav", { x: "-100px", opacity: 0 }, "initial-header+=0")
+        initial_tl.to(".nav", { x: "0px", opacity: 1, duration: 2 })
+        initial_tl.add(home_func(), "initial-header+=4")
+    } else {
+        initial_tl.from(".nav", { y: "100px", opacity: 0 }, "initial-header+=0")
+        initial_tl.to(".nav", { y: "0px", opacity: 1, duration: 2 })
+    }
 
-// animation-name: slideUp, disappear;
-// animation-duration: 1s, 0.1s;
-// animation-delay: 2s, 3.1s;
-// animation-timing-function: ease-in, linear;
-// animation-fill-mode: forwards, forwards;
+}
 
-// @keyframes fadeOut {
-//     0% {
-//         opacity: 1;
-//     } 
-//     100% {
-//         opacity: 0;
-//     }
-//   }
+// initial animation end
 
-//   @keyframes slideUp {
-//     0% {
-//         transform: translateY(0);
-//     } 
-//     100% {
-//         transform: translateY(-150px);
-//     }
-//   }
+// showcase animation start
 
-//   @keyframes disappear {
-//     to {
-//         height: 0;
-//         z-index: -100;
-//     }
-//   }
+// variables
+
+const showcase_tl = gsap.timeline({ defaults: { ease: "power2.out" } })
+
+function showcase_func() {
+    console.log('showcase')
+}
+
+// showcase animation end
+
+// skills animation start
+
+// variables
+
+const skills_tl = gsap.timeline({ defaults: { ease: "power2.out" } })
+
+function skills_func() {
+    console.log('skills')
+}
+
+// skills animation end
+
+// about animation start
+
+// variables
+
+const about_tl = gsap.timeline({ defaults: { ease: "power2.out" } })
+
+function about_func() {
+    console.log('about')
+}
+
+// about animation end
+
+// function calls
+
+loading_animation()
